997e10148fbaba8d1a27d723c5ebf719d6235bfe
camilasantosoliveiracosta <camila32-10@hotmail.com>
2024-09-04 13:00:58 -0300

    calculando o imc

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -17,21 +17,20 @@ class MainActivity : AppCompatActivity() {
         //Recuperar o texto digitado no edt peso
 
 
-
         val edtPeso = findViewById<TextInputEditText>(R.id.edittext_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edittext_altura)
 
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
-            val peso = edtPeso.text
-            val altura = edtAltura.text
-            println("Camila" + peso)
+            val peso: Float  = edtPeso.text.toString().toFloat()
+            val altura: Float = edtAltura.text.toString().toFloat()
+
+            val alturaQ2 = altura * altura
+            val resultado = peso / alturaQ2
+
+            println("Camila" + resultado)
         }
-
-
-
     }
-
 
 }
