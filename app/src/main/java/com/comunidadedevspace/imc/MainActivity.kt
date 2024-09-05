ca344be4f7cc74f152440dfbf5810f50b1a9b24f
camilasantosoliveiracosta <camila32-10@hotmail.com>
2024-09-05 14:30:38 -0300

    Dados para a próxima tela

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
@@ -10,13 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //Recuperar os componentes EditText
-        //criar uma variavel e associar (=) o componente de UI<EditText>
-        //Recuperar o botão na tela
-        //Colocar ação no botao setOnclickListener
-        //Recuperar o texto digitado no edt peso
-
 
         val edtPeso = findViewById<TextInputEditText>(R.id.edittext_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edittext_altura)
@@ -31,8 +26,6 @@ class MainActivity : AppCompatActivity() {
             val alturaStr: String = edtAltura.text.toString()
 
             if (pesoStr == "" || alturaStr == ""){
-                //mostrar mensagem para o usuário
-
 
                 Snackbar
                     .make(
@@ -48,7 +41,15 @@ class MainActivity : AppCompatActivity() {
 
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
-                println("Camila" + resultado)
+
+                //Navegar para a proxima tela
+                //criar o layout da proxima tela
+                //Passar dados  (resultado) para a proxima tela
+                //Intent - classe do proprio android
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+              startActivity(intent)
             }
         }
     }
